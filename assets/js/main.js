e15a70d420dbdb44db493366fdd33e5a61e37b58
Adyna Lima <adynalsl@gmail.com>
2023-04-21 22:59:35 -0300

    :construction:feat: adicionando os dígitos na tela da calculadora.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,32 @@ const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
 
 class Calculator {
+    constructor(previousOperationText, currentOperationText) {
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
+    }
 
+    //Adicionando dígito na tela da calculadora 
+    addDigit(digit) {
+        this.currentOperation = digit;
+        this.updateScreen();
+    }
+
+    //Alterando valores da tela da calculadora
+    updateScreen() {
+        this.currentOperationText.innerText += this.currentOperation; 
+    }
 }
+
+const calc = new Calculator(previousOperationText, currentOperationText)
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
 
         const value = e.target.innerText;
         if (+value >= 0 || value === ".") {
-            console.log(value);
+            calc.addDigit(value);
         } else {
             console.log("op" + value);
         }
